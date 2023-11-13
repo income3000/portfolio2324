22dd4e30ab73745745f02b7a1ada2baf66b1bd00
income3000 <income3000@gmail.com>
2023-11-13 00:05:12 -0500

    accomplishment css

--- src/components/Accomplishments.js
+++ src/components/Accomplishments.js
@@ -1,19 +1,83 @@
 import React from 'react'
+
 import './Accomplishments.css'
 export default function Accomplishments() {
     return (
-		<>
-        <div >
-       
-            <h2 className="sticky-top">Projects</h2>
+		
+<div className="row ">
+
+
+<div className="card">
+ <div className="col-sm-6">
+   <div className="card-body">
+     <img src="https://www.nursebuff.com/wp-content/uploads/2015/11/code-blue.png" className="card-img-top" alt="..." />
+     <h5 className="card-title">Code Blue</h5>
+     <p className="card-text">Some content.</p>
+     <a href="https://codeblue.netlify.app/" class="btn btn-primary">CODE BLUE APP</a>
+   </div>
+ </div>
+</div>
+
+
+
+  <div className="card">
+   <div className="col-sm-6">
+    <div className="card-body">
+      <img src="https://www.fontica.com/wp-content/uploads/2020/01/hollywood.jpg" className="card-img-top" alt="..." />
+	  <h5 className="card-title">Movie Info</h5>
+      <p className="card-text">Some content.</p>
+      <a href="https://mymovieinfo.netlify.app" class="btn btn-primary">MOVIE INFO APP</a>
+    </div>
+   </div>
+  </div>
+
+  <div className="card">
+   <div className="col-sm-6">
+    <div className="card-body">
+      <img src="https://www.fontica.com/wp-content/uploads/2020/01/hollywood.jpg" className="card-img-top" alt="..." />
+	  <h5 className="card-title">Movie Info</h5>
+	  <p className="card-text">Some content.</p>
+      <a href="https://mymovieinfo.netlify.app" class="btn btn-primary">MOVIE INFO APP</a>
+    </div>
+   </div>
+  </div>
+
+  <div className="card">
+   <div className="col-sm-6">
+    <div className="card-body">
+      <img src="https://www.fontica.com/wp-content/uploads/2020/01/hollywood.jpg" className="card-img-top" alt="..." />
+	  <h5 className="card-title">Movie Info</h5>
+	  <p className="card-text">Some content.</p>
+      <a href="https://mymovieinfo.netlify.app" class="btn btn-primary">MOVIE INFO APP</a>
+    </div>
+   </div>
+  </div>
+
+  <div className="card">
+ <div className="col-sm-6">
+   <div className="card-body">
+     <img src="https://www.nursebuff.com/wp-content/uploads/2015/11/code-blue.png" className="card-img-top" alt="..." />
+     <h5 className="card-title">Code Blue</h5>
+	 <p className="card-text">Some content.</p>
+     <a href="https://codeblue.netlify.app/" class="btn btn-primary">CODE BLUE APP</a>
+   </div>
+ </div>
+</div>
+
+
+
+
+            {/* <h2 className="sticky-top">Projects</h2>
             <a href="http://www.income3000.com" rel="noerror">Trivia</a>
             <br />
             <a href="https://mymovieinfo.netlify.app" rel="noerror"> Movie Finder</a>
             <br />
             <a href="https://codeblue.netlify.app/" rel="_blank" className="projetx" alt="nopage">Code blue</a>
-      
-        </div>
+       */}
+	
+
+        
 		
-		 </>
+</div>
     )
 }
